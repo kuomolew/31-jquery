52b455c024d65f06042245b9e2270d43c109bfe3
kuomolew <kuomolew@gmail.com>
2019-12-06 20:34:35 +0200

    tabs finish

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 $(document).ready( function () {
+    /**
+     * Stars rating init
+     */
+    $.ratePicker("#demo", {
+        rate : function (stars){
+        }
+      });
+    /**
+     * Tab switcer
+     */
+    $('.card__header').on('click', 'li', (e) => {                       
+        let target;
+        let previous;
     
-    console.log('test');
+        if ( !$(e.target).hasClass('card__tab') ) {
+            target = $(e.target).parents('.card__tab');
+        } else {
+            target = e.target;
+        }
+
+        if ( !$(target).hasClass('card__tab-active') ) {
+            previous = $('.card__tab-active').attr('id');               // remember previous tab id
+            $(`#card-${previous}`).addClass('hidden');                  // hide active tab content
+            $('.card__tab-active').removeClass('card__tab-active');     // stop highlight previous selected tab
+            $(target).addClass('card__tab-active');                     // highlight new selected tab
+            target = $('.card__tab-active').attr('id');                 // set target to new tab id
+            $(`#card-${target}`).removeClass('hidden');                 // show active tab content
+        }
+    });
 
 });
